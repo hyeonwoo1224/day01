9fef309fc09483d11beba58e6541b05ae859e590
hyeonwoo <ejffoddl1@naver.com>
2021-07-23 11:31:48 +0900

    Ex02 생성

---
+++ src/day01/Ex02.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		/*
+		 \n : 엔터와 비슷하다
+		 \t : tap크기만큼 오른쪽으로 이동(8칸)
+		 \" : "(큰따옴표) 표시 (일반 문자로 사용하고자 할때)
+		 \\ : \(역슬래쉬) 표현할 때 사용
+		 */
+		
+	}
+}
